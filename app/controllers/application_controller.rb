72057a5db53ca4ad9516b44ad84ff91790b99544
Jonathan Kiichi Kimura <picard33470@gmail.com>
2026-03-18 15:45:03 +0900

    Changed some user_signed_in? to either giver or accepter for new user model changes. Updated seed to match new schema

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_any!
+
+  private
+
+  def authenticate_any!
+    return if devise_controller?
+    unless giver_signed_in? || accepter_signed_in?
+      redirect_to root_path, alert: "Please sign in to continue."
+    end
+  end
 end
