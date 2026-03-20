84d5e63baf4dd57d92365b47d1d78fc6189eacfc
Jonathan Kiichi Kimura <picard33470@gmail.com>
2026-03-20 11:50:16 +0900

    Resolving merge conflict

--- test/controllers/accepters_controller_test.rb
+++ test/controllers/accepters_controller_test.rb
@@ -1,22 +1,3 @@
-require "test_helper"
-
-class GiversControllerTest < ActionDispatch::IntegrationTest
-  include Devise::Test::IntegrationHelpers
-
-  setup do
-    @giver = Giver.create!(
-      first_name: "Test Giver",
-      email: "giver@example.com",
-      password: "password123",
-      suburb: "Shinjuku"
-    )
-  end
-
-  test "should get giver show" do
-    skip "GiversController#show not yet implemented"
-  end
-end
-
 class AcceptersControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
