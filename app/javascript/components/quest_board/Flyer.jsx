2b3c6826d5e9655421cb69d5d49e1e2e7e1306f8
Koji Kaga <koji.farre@me.com>
2026-03-21 21:13:48 +0900

    flyer is simply styled and positioned on quest-board

--- app/javascript/components/quest_board/Flyer.jsx
+++ app/javascript/components/quest_board/Flyer.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 function Flyer({ quest }) {
   return (
-    <div>
-      <h3>{quest.title}</h3>
-      <span>{quest.description}</span>
-      <span>{quest.reward_amount}{quest.reward_type}</span>
+    <div className="flyer">
+      <div className="flyer__pin"></div>
+      <div className="flyer__paper">
+        <h3>{quest.title}</h3>
+        <span>{quest.description}</span>
+        <span>{quest.reward_amount}{quest.reward_type}</span>
+      </div>
     </div>
   )
 }
